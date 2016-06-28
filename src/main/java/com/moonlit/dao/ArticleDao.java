f267064405b31920a627590ad5d6108d037d0f9b
moonlightpoet <moonlightpoet@163.com>
2016-06-28 11:35:52 +0800

    create `article` model and dao

---
+++ src/main/java/com/moonlit/dao/ArticleDao.java
@@ -0,0 +1,36 @@
+package com.moonlit.dao;
+
+import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import com.moonlit.model.Article;
+
+public class ArticleDao extends JdbcDaoSupport {
+	class ArticleRowMapper implements RowMapper<Article> {
+		@Override
+		public Article mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Article article = new Article();
+			article.setId(rs.getInt("id"));
+			article.setTitle(rs.getString("title"));
+			article.setContent(rs.getString("content"));
+			return article;
+		}
+	}
+	public List<Article> queryAll() {
+		return getJdbcTemplate().query("select * from blog", new ArticleRowMapper());
+	}
+	public void create(Article article) {
+		getJdbcTemplate().update("insert into userinfo (title,content) values (?,?)",
+				new Object[] { article.getTitle(), article.getContent() });
+	}
+	public void update(Article article) {
+		getJdbcTemplate().update("update blog set title=?,content=? where id=?",
+				new Object[] { article.getTitle(), article.getContent(), article.getId() });
+	}
+	public void delete(int id) {
+		getJdbcTemplate().update("delete from blog where id=?", new Object[] { id });
+	}
+}
---
+++ src/main/java/com/moonlit/model/Article.java
@@ -0,0 +1,27 @@
+package com.moonlit.model;
+
+public class Article {
+	private int id;
+	private String title;
+	private String content;
+	public Article() {
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
